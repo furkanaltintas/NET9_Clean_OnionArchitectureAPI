6f624695e448131cedbcb7acc5713cc02655bb16
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-06 17:23:48 +0300

    ReadRepository işlemleri yapıldı.

---
+++ Core/OnionDemo.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -0,0 +1,5 @@
+﻿namespace OnionDemo.Application.Interfaces.Repositories;
+
+public interface IWriteRepository
+{
+}
---
+++ Infrastructure/OnionDemo.Persistence/Repositories/WriteRepository.cs
@@ -0,0 +1,5 @@
+﻿namespace OnionDemo.Persistence.Repositories;
+
+public class WriteRepository
+{
+}
