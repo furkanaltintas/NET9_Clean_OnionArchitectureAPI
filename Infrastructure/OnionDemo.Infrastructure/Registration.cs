b656ee3c3014df2ec01bb0555dd037c286625e07
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-08 17:55:32 +0300

    TokenService işlemleri yapıldı.

--- Infrastructure/OnionDemo.Infrastructure/Registration.cs
+++ Infrastructure/OnionDemo.Infrastructure/Registration.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using OnionDemo.Application.Interfaces.Tokens;
 using OnionDemo.Infrastructure.Tokens;
+using System.Text;
 
 namespace OnionDemo.Infrastructure;
 
@@ -10,5 +14,25 @@ public static class Registration
         this IServiceCollection services, IConfiguration configuration)
     {
         services.Configure<TokenSettings>(configuration.GetSection("JWT"));
+        services.AddTransient<ITokenService, TokenService>();
+
+        services.AddAuthentication(opt =>
+        {
+            opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opt =>
+        {
+            opt.SaveToken = true;
+            opt.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SecretKey"])),
+                ValidateLifetime = false,
+                ValidIssuer = configuration["JWT:Issuer"],
+                ValidAudience = configuration["JWT:Audience"],
+                ClockSkew = TimeSpan.Zero
+            };
+        });
     }
 }
