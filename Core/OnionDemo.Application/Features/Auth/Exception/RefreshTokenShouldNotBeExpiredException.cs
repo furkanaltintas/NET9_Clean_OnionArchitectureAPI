1617391699f29ae0d3a5708dc5e4c2473ea5ea95
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-08 22:39:15 +0300

    RefreshToken işlemleri yapıldı.

---
+++ Core/OnionDemo.Application/Features/Auth/Exception/RefreshTokenShouldNotBeExpiredException.cs
@@ -0,0 +1,11 @@
+﻿using OnionDemo.Application.Bases;
+
+namespace OnionDemo.Application.Features.Auth.Exception
+{
+    public class RefreshTokenShouldNotBeExpiredException : BaseExceptions
+    {
+        public RefreshTokenShouldNotBeExpiredException() : base("Oturum süresi sona ermiştir. Lütfen tekrar giriş yapınız.")
+        {
+        }
+    }
+}
