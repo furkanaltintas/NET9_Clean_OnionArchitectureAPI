62be7d977e518e3f52715476ae0bd876e597840a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-08 21:36:25 +0300

    Login işlemleri yapıldı.

---
+++ Core/OnionDemo.Application/Features/Auth/Command/Login/LoginCommandHandler.cs
@@ -0,0 +1,62 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using OnionDemo.Application.Bases;
+using OnionDemo.Application.Features.Auth.Rules;
+using OnionDemo.Application.Interfaces.AutoMapper;
+using OnionDemo.Application.Interfaces.Tokens;
+using OnionDemo.Application.Interfaces.UnitOfWorks;
+using OnionDemo.Domain.Entities;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace OnionDemo.Application.Features.Auth.Command.Login;
+
+public class LoginCommandHandler : BaseHandler, IRequestHandler<LoginCommandRequest, LoginCommandResponse>
+{
+    private readonly UserManager<User> _userManager;
+    private readonly IConfiguration _configuration;
+    private readonly ITokenService _tokenService;
+    private readonly AuthRules _authRules;
+
+    public LoginCommandHandler(IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor, UserManager<User> userManager, AuthRules authRules, ITokenService tokenService, IConfiguration configuration) : base(mapper, unitOfWork, httpContextAccessor)
+    {
+        _configuration = configuration;
+        _tokenService = tokenService;
+        _userManager = userManager;
+        _authRules = authRules;
+    }
+
+    public async Task<LoginCommandResponse> Handle(LoginCommandRequest request, CancellationToken cancellationToken)
+    {
+        var user = await _userManager.FindByEmailAsync(request.Email);
+        var checkPassword = await _userManager.CheckPasswordAsync(user, request.Password);
+
+        await _authRules.EmailOrPasswordShouldNotBeInvalid(user, checkPassword);
+
+        IList<string> roles = await _userManager.GetRolesAsync(user);
+
+        JwtSecurityToken token = await _tokenService.CreateToken(user, roles);
+        string refreshToken = _tokenService.GenerateRefreshToken();
+
+        _ = int.TryParse(_configuration["JWT:RefreshTokenValidityInDays"], out int refreshTokenValidityInDays);
+
+        user.RefreshToken = refreshToken;
+        user.RefreshTokenExpiryTime = DateTime.Now.AddDays(refreshTokenValidityInDays);
+
+        await _userManager.UpdateAsync(user);
+        await _userManager.UpdateSecurityStampAsync(user);
+
+        string _token = new JwtSecurityTokenHandler().WriteToken(token);
+
+
+        await _userManager.SetAuthenticationTokenAsync(user, "Default", "AccessToken", _token);
+
+        return new()
+        {
+            Token = _token,
+            RefreshToken = refreshToken,
+            Expiration = token.ValidTo
+        };
+    }
+}
---
+++ Core/OnionDemo.Application/Features/Auth/Command/Login/LoginCommandResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace OnionDemo.Application.Features.Auth.Command.Login;
+
+public class LoginCommandResponse
+{
+    public string Token { get; set; }
+    public string RefreshToken { get; set; }
+    public DateTime Expiration { get; set; }
+}
